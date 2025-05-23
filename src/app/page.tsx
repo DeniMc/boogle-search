bfdabecb6a86cfa28be1c9d7fd714926720a9a6f
Denis McArdle <denimcardle@gmail.com>
2025-05-24 00:56:41 +0100

    add body section to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from '@/components/HomeSearch';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
     <HomeHeader />
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          src="/images/doogle-icon.png"
+          alt="Doogle Logo"
+          width={500}
+          height={150}
+          priority
+          style={{width: "auto", height: "auto"}}
+
+        />
+        <HomeSearch />
+      </div>
     </>
   )
 }
